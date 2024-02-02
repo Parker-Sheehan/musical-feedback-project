e40449ea907adfc3116f8ad4868b561a7f47f133
Parker Sheehan <Parkersheehan007@gmail.com>
2024-02-02 15:51:14 -0600

    centering div nonsense

--- src/components/main/ProfileInfo.tsx
+++ src/components/main/ProfileInfo.tsx
@@ -1,98 +1,97 @@
 import "./ProfileInfo.css";
 import { FC, useState } from "react";
-import MyVerticallyCenteredModal from '../ui/MyVerticallyCenteredModal'
+import MyVerticallyCenteredModal from "../ui/MyVerticallyCenteredModal";
 
 import { ProfileData } from "../views/ProfilePage";
 
 interface ProfileDataProp {
   profileData: ProfileData;
-  setProfileDataHandler : (profileDataObj: ProfileData) => void
+  setProfileDataHandler: (profileDataObj: ProfileData) => void;
 }
 
-const ProfileInfo: FC<ProfileDataProp> = ({ profileData, setProfileDataHandler}) => {
+const ProfileInfo: FC<ProfileDataProp> = ({
+  profileData,
+  setProfileDataHandler,
+}) => {
   console.log(profileData);
 
-  const [togglePfpModal,setTogglePfpModal] = useState<boolean>(false)
-  
+  const [togglePfpModal, setTogglePfpModal] = useState<boolean>(false);
+
   const handleProfilePictureClick = () => {
     setTogglePfpModal(true);
   };
 
-  let genreArray: string[] = (profileData.genres.map((genre: any) => {
-    return (genre.genreName)
-  }));
+  let genreArray: string[] = profileData.genres.map((genre: any) => {
+    return genre.genreName;
+  });
 
   return (
     <>
-        <MyVerticallyCenteredModal
+      <MyVerticallyCenteredModal
         show={togglePfpModal}
         onHide={() => setTogglePfpModal(false)}
         setProfileDataHandler={setProfileDataHandler}
         profileData={profileData}
       />
-    <div id="profile-info-card">
-      <div id="left-profile" className="inside-profile-card">
-        <div
-          id="profile-picture"
-          style={{
-            backgroundImage: "url(" + profileData.profilePicture + ")",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-          onClick={handleProfilePictureClick}
-        ></div>
-        <div id="stats-container">
-          <div>
-            <h3 className="top-text">Followers</h3>
-            <h3>100</h3>
-          </div>
-          <div className="inside-stat-container">
-            <h3>Following</h3>
-            <h3>120</h3>
-          </div>
-          <div className="inside-stat-container">
-            <h3>Tracks</h3>
-            <h3>10</h3>
+      <div id="profile-info-card">
+        <div id="left-profile" className="inside-profile-card">
+          <div
+            id="profile-picture"
+            className="bg-cover bg-center bg-no-repeat"
+            style={{ backgroundImage: `url(${profileData.profilePicture})` }}
+            onClick={handleProfilePictureClick}
+          ></div>
+          <div id="stats-container">
+            <div>
+              <h3 className="top-text">Followers</h3>
+              <h3>100</h3>
+            </div>
+            <div className="inside-stat-container">
+              <h3>Following</h3>
+              <h3>120</h3>
+            </div>
+            <div className="inside-stat-container">
+              <h3>Tracks</h3>
+              <h3>10</h3>
+            </div>
           </div>
         </div>
-      </div>
-      <div id="right-profile" className="inside-profile-card">
-        <h1>{profileData.displayName}</h1>
-        <div id="genre-container">
-          <h3>Genre</h3>
-          <h3>{genreArray}</h3>
-        </div>
-        <div id="ratings-container">
-          <div id="total-stars-container">
-            <h2>Total Score</h2>
-            <h2>3.2</h2>
+        <div id="right-profile" className="inside-profile-card">
+          <h1>{profileData.displayName}</h1>
+          <div id="genre-container">
+            <h3>Genre</h3>
+            <h3>{genreArray}</h3>
           </div>
-          <div id="specific-score-container">
-            <div className="specific-score" id="musicality-score">
-              <h3>musicality</h3>
-              <h3>3.6</h3>
+          <div id="ratings-container">
+            <div id="total-stars-container">
+              <h2>Total Score</h2>
+              <h2>3.2</h2>
             </div>
-            <div className="specific-score" id="rhythm-score">
-              <h3>rhythm</h3>
-              <h3>3.8</h3>
-            </div>
-            <div className="specific-score" id="sound-design-score">
-              <h3>sound design</h3>
-              <h3>4</h3>
-            </div>
-            <div className="specific-score" id="arrangment-score">
-              <h3>arrangment</h3>
-              <h3>2.8</h3>
-            </div>
-            <div className="specific-score" id="mixing/master-score">
-              <h3>mixing/master</h3>
-              <h3>2.4</h3>
+            <div id="specific-score-container">
+              <div className="specific-score" id="musicality-score">
+                <h3>musicality</h3>
+                <h3>3.6</h3>
+              </div>
+              <div className="specific-score" id="rhythm-score">
+                <h3>rhythm</h3>
+                <h3>3.8</h3>
+              </div>
+              <div className="specific-score" id="sound-design-score">
+                <h3>sound design</h3>
+                <h3>4</h3>
+              </div>
+              <div className="specific-score" id="arrangment-score">
+                <h3>arrangment</h3>
+                <h3>2.8</h3>
+              </div>
+              <div className="specific-score" id="mixing/master-score">
+                <h3>mixing/master</h3>
+                <h3>2.4</h3>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
--- src/components/views/Splash.tsx
+++ src/components/views/Splash.tsx
@@ -9,44 +9,57 @@ import { FC, useState } from "react";
 //   setProfileDataHandler : (profileDataObj: ProfileData) => void
 // }
 
-const ProfileInfo= () => {
+const ProfileInfo = () => {
   // console.log(profileData);
 
   // const [togglePfpModal,setTogglePfpModal] = useState<boolean>(false)
-  
+
   // const handleProfilePictureClick = () => {
   //   setTogglePfpModal(true);
   // };
 
-  let genreArray: string[] = [
-    'drum and bass',
-    'trap',
-    'house'
-  ]
+  let genreArray: string[] = ["drum and bass", "trap", "house"];
 
   return (
     <>
-        {/* <MyVerticallyCenteredModal
+      {/* <MyVerticallyCenteredModal
         show={togglePfpModal}
         onHide={() => setTogglePfpModal(false)}
         setProfileDataHandler={setProfileDataHandler}
         profileData={profileData}
       /> */}
-    <div className="m-10 w-3/4 h-3/4 rounded-lg grid grid-rows-4 grid-cols-10 gap-3">
-      <div className="bg-background2 grid-cols-subgrid col-span-5 grid-row-subgrid row-span-2 rounded-lg grid grid-cols-5 gap-1">
-        <div className="grid-row-subgrid row-span-2 grid-cols-subgrid col-span-3 rounded-lg"></div>
-        <div className="bg-background3 grid-row-subgrid row-span-1 grid-cols-subgrid col-span-2 rounded-tr-lg"></div>
-        <div className="bg-background3 grid-row-subgrid row-span-1  grid-cols-subgrid col-span-2 rounded-br-lg"></div>
-        
+      <div className="m-10 w-3/4 h-3/4 rounded-lg grid grid-rows-4 grid-cols-10 gap-3">
+        <div className="bg-background2 grid-cols-subgrid col-span-5 grid-row-subgrid row-span-2 rounded-lg grid grid-cols-5 gap-1">
+          <div className="grid-row-subgrid row-span-2 grid-cols-subgrid col-span-3 rounded-lg flex flex-col justify-center items-center gap-3">
+            <div
+              className="bg-cover bg-center bg-no-repeat size-36 rounded-full mb-2"
+              style={{
+                backgroundImage: `url(https://wallpapers-clan.com/wp-content/uploads/2022/02/hunter-x-hunter-killua-pfp-1.jpg)`,
+              }}
+            ></div>
+            <button className="h-8 w-48 bg-accent flex justify-center items-center rounded-full  text-text ">
+              Follow
+            </button>
+            <button className="h-8 w-48 bg-accent rounded-full text-text">
+              Message
+            </button>
+          </div>
+          <div className="bg-background3 grid-row-subgrid row-span-1 grid-cols-subgrid col-span-2 rounded-tr-lg text-text flex flex-col justify-center items-center">
+            <h1 className="text-2xl">Followers</h1>
+            <h2 className="text-xl">180</h2>
+          </div>
+          <div className="bg-background3 grid-row-subgrid row-span-1 grid-cols-subgrid col-span-2 rounded-tr-lg text-text flex flex-col justify-center items-center">
+            <h1 className="text-2xl">Following</h1>
+            <h2 className="text-xl">180</h2>
+          </div>       
+          </div>
+        <div className="bg-background2 grid-cols-subgrid col-span-3 grid-row-subgrid row-span-2 rounded-lg"></div>
+        <div className="bg-gradient-to-br from-prim to-accent grid-cols-subgrid col-span-2 grid-row-subgrid row-span-4  rounded-lg"></div>
+        <div className="bg-sec2 grid-cols-subgrid col-span-3 grid-row-subgrid row-span-2 rounded-lg"></div>
+        <div className="bg-sec2  grid-cols-subgrid col-span-3 grid-row-subgrid row-span-2 rounded-lg"></div>
+        <div className="bg-sec2 grid-cols-subgrid col-span-2 rounded-lg"></div>
+        <div className="bg-sec2 grid-cols-subgrid col-span-2 rounded-lg"></div>
       </div>
-      <div className="bg-background2 grid-cols-subgrid col-span-3 grid-row-subgrid row-span-2 rounded-lg"></div>
-      <div className="bg-gradient-to-br from-prim to-accent grid-cols-subgrid col-span-2 grid-row-subgrid row-span-4  rounded-lg"></div>
-      <div className="bg-sec2 grid-cols-subgrid col-span-3 grid-row-subgrid row-span-2 rounded-lg"></div>
-      <div className="bg-sec2  grid-cols-subgrid col-span-3 grid-row-subgrid row-span-2 rounded-lg"></div>
-      <div className="bg-sec2 grid-cols-subgrid col-span-2 rounded-lg"></div>
-      <div className="bg-sec2 grid-cols-subgrid col-span-2 rounded-lg"></div>
-
-    </div>
     </>
   );
 };
