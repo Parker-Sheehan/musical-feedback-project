67b987b0151d932fc6b3db4823194d428b193900
Parker Sheehan <Parkersheehan007@gmail.com>
2024-02-07 16:21:46 -0600

    styling song cards

--- src/components/main/NavBar.tsx
+++ src/components/main/NavBar.tsx
@@ -33,19 +33,6 @@ const NavBar = () => {
   console.log(loggedInUser.userId);
   return (
     <>
-    <Alert variant="success">
-      <Alert.Heading>Hey, nice to see you</Alert.Heading>
-      <p>
-        Aww yeah, you successfully read this important alert message. This
-        example text is going to run a bit longer so that you can see how
-        spacing within an alert works with this kind of content.
-      </p>
-      <hr />
-      <p className="mb-0">
-        Whenever you need to, be sure to use margin utilities to keep things
-        nice and tidy.
-      </p>
-    </Alert>
       {
         !loggedInUser.userId &&
         <nav className="flex items-center justify-around h-12 w-full bg-background-100 border-y-2 border-background2">
--- src/components/main/SongCard.tsx
+++ src/components/main/SongCard.tsx
@@ -10,27 +10,20 @@ interface SongCardProps {
 const SongCard: FC<SongCardProps> = ({ song }) => {
   // console.log(song);
   return (
-    <div className="song-card-block">
-      {/* <div
-        className="song-card"
-        style={{
-          background: `url(https://i1.sndcdn.com/artworks-000189983181-azrg5q-t500x500.jpg)`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <h2>{song.title}</h2>
-        <Link to={"/SongProfilePage/" + song.songId} state={{ songObj: song }}>
-          <button className="view-critique-button">View Song</button>
-        </Link>
-      </div> */}
-      <div className="song-card">
-        <div className="song-card-header">
-        <h2>{song.title}</h2>
-        <Link to={"/SongProfilePage/" + song.songId} state={{ songObj: song }}>
-          <button className="view-critique-button">View Song</button>
-        </Link>
+    <div className="w-1/2 h-full flex justify-center items-center p-4">
+      <div className="w-4/5 h-4/5 bg-sec flex flex-col justify-between p-4 rounded-lg">
+        <div className=" flex justify-around">
+          <h2 className="text-text font-heading">{song.title}</h2>
+          <Link
+            to={"/SongProfilePage/" + song.songId}
+            state={{ songObj: song }}
+            className="w-1/4"
+          >
+            <button className="bg-accent h-3/4 w-full rounded text-black ">
+              View Song
+            </button>
+          </Link>
+          <button className="bg-accent h-3/4 w-1/4 rounded">View Song</button>
         </div>
         <iframe
           width="100%"
