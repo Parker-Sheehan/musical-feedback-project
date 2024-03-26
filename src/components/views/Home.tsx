6bf07e232a919258e4279895465fa9bb8685073c
Parker Sheehan <Parkersheehan007@gmail.com>
2024-03-25 19:37:21 -0500

    starting splash

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -14,9 +14,14 @@ const Home = () => {
     )
     : 
     (
-        <h1>
-            splash
-        </h1>
+        <main className=' size-full flex justify-center items-center'>
+          <div className='h-full w-1/2 bg-red-400'>
+          
+          <div className='relative left-12 top-12 w-[36rem] bg-prim aspect-square rounded-xl'>
+          <div className='relative left-1/2 top-1/2 w-[24rem] bg-sec aspect-square rounded-xl'></div>
+          </div>
+          </div>
+        </main>
     )}
     </>
   )
