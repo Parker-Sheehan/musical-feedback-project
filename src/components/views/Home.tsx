49159190767f3b92646d578c1fc30cbc9ab1c06f
Parker Sheehan <Parkersheehan007@gmail.com>
2024-03-29 18:16:22 -0500

    moble spash up

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -14,13 +14,21 @@ const Home = () => {
     )
     : 
     (
-        <main className=' size-full flex justify-center items-center'>
-          <div className='h-full w-1/2 bg-red-400'>
+        <main className=' size-full'>
+          <div className='h-3/4 w-full pl-4 flex flex-col justify-evenly'>
           
-          <div className='relative left-12 top-12 w-[36rem] bg-prim aspect-square rounded-xl'>
-          <div className='relative left-1/2 top-1/2 w-[24rem] bg-sec aspect-square rounded-xl'></div>
+          <div className=' md:left-12 w-5/6 bg-prim aspect-square rounded-xl'>
+          <div className='relative left-1/3 top-2/3 w-10/12 bg-sec aspect-square rounded-xl'>
+            <h1 className=' relative -left-20  -top-20 font-heading text-black text-6xl'>Melotika</h1>
+            <h1 className=' relative -left-16  -top-24 whitespace-nowrap font-heading text-black text-2xl'>Social Platform</h1>
+            <h1 className=' relative -left-14  -top-28 whitespace-nowrap  font-heading text-black text-2xl'>For Music Producers</h1>
+
+
+
           </div>
           </div>
+          </div>
+          <div></div>
         </main>
     )}
     </>
