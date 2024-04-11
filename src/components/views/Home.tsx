740fd5da6566b8c053b4046568a8edb7ff44053f
Parker Sheehan <Parkersheehan007@gmail.com>
2024-04-11 11:04:22 -0500

    worked on splash

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -15,10 +15,10 @@ const Home = () => {
     : 
     (
         <main className=' size-full'>
-          <div className='h-3/4 w-full pl-4 flex flex-col justify-evenly'>
+          <div className='h-3/4 w-full pl-4 flex flex-col justify-evenly sm:items-center'>
           
-          <div className=' md:left-12 w-5/6 bg-prim aspect-square rounded-xl'>
-          <div className='relative left-1/3 top-2/3 w-10/12 bg-sec aspect-square rounded-xl'>
+          <div className=' sm:left-12 w-5/6 sm:w-3/5 lg:w-2/5 xl:w-3/5 xl:h-5/6 bg-prim aspect-square xl:aspect-auto rounded-xl'>
+          <div className='relative left-1/3 top-2/3 w-4/5 lg:w-4/5 xl:w-4/5 xl:h-4/5 bg-sec aspect-square xl:aspect-auto rounded-xl'>
             <h1 className=' relative -left-20  -top-20 font-heading text-black text-6xl'>Melotika</h1>
             <h1 className=' relative -left-16  -top-24 whitespace-nowrap font-heading text-black text-2xl'>Social Platform</h1>
             <h1 className=' relative -left-14  -top-28 whitespace-nowrap  font-heading text-black text-2xl'>For Music Producers</h1>
